0cab96f426d81939e73491fe9506a9251fd3b01c
emsweetland <emmaksweetland@gmail.com>
2022-08-13 17:20:33 -0500

    GalleryList component draft

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'; //for style
 import axios from 'axios'; //for routing
+import GalleryList from '../GalleryList/GalleryList'
 
 
 
 function App() {
+  
+  // starting with empty array, passed to GalleryList.jsx
+  let [galleryList, setGalleryList] = useState([]);
 
-let [galleryItem, setGalleryItem] = useState([])
-
-    // run the component
+    // run when component is first put on DOM
     useEffect(() => {
     getGallery();
   });
@@ -16,9 +18,9 @@ let [galleryItem, setGalleryItem] = useState([])
 
   //GET via axios... ty getGallery
   const getGallery = () => {
-    axios.get('/gallery')
+    axios.get('/gallery') //via /gallery
     .then((response) => {
-      console.log(response.data);
+      console.log(response.data); // receive data from server
     }).catch((err) => {
       alert('couldn\'t get gallery, sorry');
       console.log('error', err);
@@ -28,9 +30,9 @@ let [galleryItem, setGalleryItem] = useState([])
 
   //PUT via axios.. count those likes
   const countLikes = (item, affection) => {
-    axios.put(`/gallery/likes/${item.id}`, {likes:item.likes})
+    axios.put(`/gallery/likes/${item.id}`, {likes:item.likes}) //target an items likes via id then change likes
     .then((response) => {
-      getGallery();
+      getGallery(); //get updated data right away!
     }).catch((err) => {
       alert('couldnt like');
       console.log('error updating likes', err);
@@ -44,6 +46,9 @@ let [galleryItem, setGalleryItem] = useState([])
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <GalleryList
+          galleryList={galleryList}
+          countLikes={countLikes}/>
       </div>
     );
 }
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,28 @@
+//GalleryList is like the table of the database
+    //via App.jsx //galleryList is the array, countLikes contains axios PUT function
+function GalleryList({ galleryList, countLikes}) {
+
+    return(
+        <div>
+        <h2>my gallery</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>a photo :)</th>
+                </tr>
+            </thead>
+            <tbody>
+                {galleryList.map((image) => (
+                    <GalleryItem
+                        key={image.id}
+                        image={image}
+                        countLikes={countLikes}/>
+                ))} 
+            </tbody>
+        </table>
+        </div>
+    );
+}
+
+export default GalleryList; 
+//now other components may reference this one once it's been imported to App.jsx
